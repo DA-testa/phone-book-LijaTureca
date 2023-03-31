21d2fd5e31e4c60ace56c73d02dd4439717b9050
LijaTureca <124587378+LijaTureca@users.noreply.github.com>
2023-03-31 21:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,44 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    #contacts = []
+    hash_struct={}
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
+#             for contact in contacts:
+#                 if contact.number == cur_query.number:
+#                     contact.name = cur_query.name
+#                     break
+            for key, value in hash_struct.items():
+                if value == cur_query.number:
+                   hash_struct[key] = cur_query.name 
             else: # otherwise, just add it
-                contacts.append(cur_query)
+#                 contacts.append(cur_query)
+                  hash_struct[cur_query.name]=cur_query.number
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
+#             for j in range(len(contacts)):
+#                 if contacts[j].number == cur_query.number:
+#                     contacts.pop(j)
+#                     break
+            for key, value in hash_struct.items():
+                if value==cur_query.number:
+                    del hash_struct[key]
                     break
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+#             response = 'not found'
+#             for contact in contacts:
+#                 if contact.number == cur_query.number:
+#                     response = contact.name
+#                     break
+#             result.append(response)
+             response = "not found"
+             for key, value in hash_struct.items():
+                 if value == cur_query.number:
+                     response = key
+                     break
+             result.append(response)
     return result
 
 if __name__ == '__main__':
